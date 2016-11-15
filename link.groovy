866bc2937145325de46414a2b6173f3bc6cff082
Andrew Turley <aturley123@gmail.com>
2016-11-14 22:04:15 -0500

    Added servo and adjusted sizes accordingly

--- link.groovy
+++ link.groovy
@@ -1,7 +1,7 @@
 import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
 import eu.mihosoft.vrl.v3d.parametrics.*;
 
-CSG bottom = new Cube(50, 120, 30).toCSG()
+CSG bottom = new Cube(50, 120, 5).toCSG().movez(12.5)
 CSG top = new Cube(50, 120, 30).toCSG().movez(30)
 CSG horn = new Cube(45, 45, 5).toCSG().movey(-30).movez(15)
 
@@ -11,10 +11,12 @@ CSG screw2 = screw.movex(-20).movey(50)
 CSG screw3 = screw.movex(-20).movey(-50)
 CSG screw4 = screw.movex(20).movey(-50)
 
+CSG servo = Vitamins.get("hobbyServo", "towerProMG91").movey(-30)
+
 ArrayList<CSG> allCad = []
 
 top = top.difference(screw1).difference(screw2).difference(screw3).difference(screw4).difference(horn)
-bottom = bottom.difference(screw1).difference(screw2).difference(screw3).difference(screw4).difference(horn)
+bottom = bottom.difference(screw1).difference(screw2).difference(screw3).difference(screw4).difference(horn).difference(servo)
 
 allCad.add(top.movez(50))
 allCad.add(bottom)
@@ -22,5 +24,6 @@ allCad.add(screw1.movez(120))
 allCad.add(screw2.movez(120))
 allCad.add(screw3.movez(120))
 allCad.add(screw4.movez(120))
+allCad.add(servo)
 
 return allCad
