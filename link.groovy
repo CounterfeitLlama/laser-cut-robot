1dd3fc5683fbb77be6dc8fb8426651b36987fd4f
Andrew Turley <aturley123@gmail.com>
2016-11-14 20:36:30 -0500

    Started work on the link

--- link.groovy
+++ link.groovy
@@ -1,3 +1,7 @@
-CSG bottom = new Cube(40, 120, 30).toCSG()
-CSG top = new Cube(40, 120, 30).toCSG().movez(30)
+CSG bottom = new Cube(50, 120, 30).toCSG()
+CSG top = new Cube(50, 120, 30).toCSG().movez(30)
+CSG horn = new Cube(45, 45, 5).toCSG().movey(-30).movez(15)
+
+top = top.difference(horn).movez(30)
+bottom = bottom.difference(horn)
 return [bottom, top]
